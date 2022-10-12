7d38f27385b83a8ea6c60e1d322d11d7df7f2202
Sergick12 <cepgruddd1@gmail.com>
2022-10-12 17:59:57 +0300

    ME-1/refactoring

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,268 +1,148 @@
 # frozen_string_literal: true
 
-# Arraycompleted: all? any? include? none?, map, size, count, length, find_index
+# Array completed: all? any? include? none?, map, size, count, length, find_index
 # select, find, find_all, reject, max, min
-#
-# Hash:
-# not completed: all? any? include? none? each map! map
-# find_all find_index reject max min size count length select find
 
 module MyEnumerable
   def my_all?(value = nil)
-    case
-    when value.nil? && !block_given?
-      each do |item|
-        return false unless item
-      end
-      true
-    when !value.nil?
-      each do |item|
-        return false unless value === item
-      end
-      true
-    when block_given?
-      each do |item|
-        return false unless yield(item)
-      end
-      true
+    if value.nil? && !block_given?
+      each { |item| return false unless item }
+    elsif !value.nil?
+      each { |item| return false unless value === item }
     else
-      "Error: value has an invalid value (#{value})"
+      each { |item| return false unless yield(item) }
     end
+    true
   end
 
   def my_any?(value = nil)
-    case
-    when value.nil? && !block_given?
-
-      return false unless size.positive?
-
-      true
-    when !value.nil?
-
-      return false unless size.positive?
-
-      each do |item|
-        return true if value === item
-      end
-      false
-    when block_given?
-      each do |item|
-        return false unless yield(item)
-      end
-      true
+    if value.nil? && !block_given?
+      return true unless size.zero?
+    elsif !value.nil?
+      each { |item| return true if value === item }
     else
-      "Error: value has an invalid value (#{value})"
+      each { |item| return true if yield(item) }
     end
+    false
   end
 
   def my_include?(value = nil)
-    case
-    when !value.nil?
-
-      return false unless size.positive?
-
-      each do |item|
-        return true if value === item
-      end
-      false
-    else
-      "Error: value has an invalid value (#{value})"
-    end
+    each { |item| return true if value === item } unless value.nil?
+    false
   end
 
   def my_none?(value = nil)
-    case
-    when value.nil? && !block_given?
-
-      return true unless size.positive?
-
-      each do |item|
-        return true unless item
-      end
-      false
-    when !value.nil?
-
-      return true unless size.positive?
-
-      each do |item|
-        return false if value === item
-      end
-      true
-    when block_given?
-      each do |item|
-        return false unless yield(item)
-      end
-      true
+    if value.nil? && !block_given?
+      each { |item| return false if item }
+    elsif !value.nil?
+      each { |item| return false if value === item }
     else
-      "Error: value has an invalid value (#{value})"
+      each { |item| return false if yield(item) }
     end
+    true
   end
 
   def my_map
-    case
-    when block_given?
+    return to_enum(:my_map) unless block_given?
 
-      return false unless size.positive?
-
-      newArray = []
-      each do |item|
-        newArray << yield(item)
-      end
-      newArray
-    else
-      'Error: value has an invalid block given'
-    end
+    new_arr = []
+    each { |item| new_arr << yield(item) }
+    new_arr
   end
 
   def my_size
     size = 0
-    each do |_item|
-      size += 1
-    end
+    each { size += 1 }
     size
   end
 
-  def my_count
-    size = 0
-    each do |_item|
-      size += 1
+  def my_count(value = nil)
+    count = 0
+    if value.nil? && !block_given?
+      each { count += 1 }
+    elsif !value.nil?
+      each { |item| count += 1 if value == item }
+    else
+      each { |item| count += 1 if yield(item) }
     end
-    size
+    count
   end
 
   def my_length
     size = 0
-    each do |_item|
-      size += 1
-    end
+    each { size += 1 }
     size
   end
 
   def my_select
-    if block_given?
-      newArray = []
-      each do |item|
-        newArray << item if yield(item)
-      end
-      newArray
-    else
-      'Error: value has an invalid block given'
-    end
+    return to_enum(:my_select) unless block_given?
+
+    map { |item| item if yield(item) } - [nil]
   end
 
-  # невдуплил про if_none_proc
   def my_find(_if_none_proc = nil)
-    case
-    when block_given?
-      each do |item|
-        return item if yield(item)
-      end
-      false
-    else
-      'Error: value has an invalid block given'
-    end
+    return to_enum(:my_find) unless block_given?
+
+    each { |item| return item if yield(item) }
+    false
   end
 
   def my_find_all
-    if block_given?
-      newArray = []
-      each do |item|
-        newArray << item if yield(item)
-      end
-      newArray
-    else
-      'Error: value has an invalid block given'
-    end
+    return to_enum(:my_find_all) unless block_given?
+
+    map { |item| item if yield(item) } - [nil]
   end
 
   def my_find_index(value = nil)
-    case
-    when !value.nil? && !block_given?
-      i = 0
-      each do |item|
-        return i if item == value
 
-        i += 1
+    if !value.nil?
+      index = 0
+      each do |item|
+        return index if item == value
+
+        index += 1
       end
-      false
-    when block_given?
+    elsif block_given?
       i = 0
       each do |item|
         return i if yield(item)
-
         i += 1
       end
-      false
-    else
-      'Error: value has an invalid block given'
     end
+    to_enum(:my_find_index)
   end
-
+  
   def my_reject
-    if block_given?
-      newArray = []
-      each do |item|
-        newArray << item unless yield(item)
-      end
-      newArray
-    else
-      'Error: value has an invalid block given'
-    end
+    return to_enum(:my_reject) unless block_given?
+
+    map { |item| item unless yield(item) } - [nil]
   end
 
   def my_min(value = nil)
-    case
-    when value.nil?
-      return nil unless size.positive?
-
-      minValue = self[0]
-      each do |_item|
-        each do |i|
-          minValue = i if minValue > i
-        end
-      end
-      minValue
-    when !value.nil?
-
-      return [] unless size.positive?
-
+    if value.nil?
+      min_value = self[0]
+      each { |i| min_value = i if min_value > i }
+      min_value
+    else
       arr = self
-      minArr = []
-      i = 0
-      while i < value
-        minArr << arr.my_min
+      (0...value).map do
+        arr.my_min
         arr.delete(arr.my_min)
-        i += 1
       end
-      minArr
     end
   end
 
-  def my_max
-    return nil unless my_size.positive?
-
-    case
-    when value.nil?
-      manValue = self[0]
-      each do |_item|
-        each do |i|
-          manValue = i if manValue < i
-        end
-      end
-      manValue
-    when !value.nil?
-
-      return [] unless size.positive?
-
+  def my_max(value = nil)
+    if value.nil?
+      max_value = self[0]
+      each { |i| max_value = i if max_value < i }
+      max_value
+    else
       arr = self
-      minArr = []
-      i = 0
-      while i < value
-        minArr << arr.my_min
-        arr.delete(arr.my_min)
-        i += 1
+      (0...value).map do
+        arr.my_max
+        arr.delete(arr.my_max)
       end
-      minArr
     end
   end
 end
